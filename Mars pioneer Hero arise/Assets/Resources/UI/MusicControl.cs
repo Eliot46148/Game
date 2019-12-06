addd24b60f0aacfe21e4b7f61af51bb0e2cb7d2c
ChiuYiKai <t106590008@ntut.org.tw>
2019-12-06 14:46:21 +0800

    BGM test not can work

--- Mars pioneer Hero arise/Assets/Resources/UI/MusicControl.cs
+++ Mars pioneer Hero arise/Assets/Resources/UI/MusicControl.cs
@@ -7,13 +7,17 @@ public class MusicControl : MonoBehaviour
     private static AudioSource audioSource;
     private bool muteState;
     private float preVolume;
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+
+        if (audioSource .isPlaying ) return;
         DontDestroyOnLoad(audioSource);
         audioSource.volume = 0.5f;
         muteState = false;
         preVolume = audioSource.volume;
+
     }
 
     public void VolumeChanged(float newVolume)

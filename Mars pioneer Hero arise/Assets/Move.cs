081e2db31cd1c24199aa8e8a42998c41d072b0be
Eliot_Cheng <eliot46148@gmail.com>
2019-12-13 23:13:01 +0800

    還原 Move.cs

--- Mars pioneer Hero arise/Assets/Move.cs
+++ Mars pioneer Hero arise/Assets/Move.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour {
+public class Move : MonoBehaviour
+{
 
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
-    public Animator anim;
-
-    float angle;
-
-    Quaternion targetrotation;
 
     void Start()
     {
@@ -23,23 +19,14 @@ public class Move : MonoBehaviour {
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
-            float v = Input.GetAxis("Vertical");
-            float h = Input.GetAxis("Horizontal");
-            /*moveDirection = new Vector3(h, 0, v);
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;*/
+            moveDirection *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
-            CalculateDirection(h, v);
-            Rotate();
         }
         moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(transform.forward*speed);
-        anim.SetInteger("state", 1);
-
-        
-
-
+        controller.Move(moveDirection * Time.deltaTime);
 
         /*if(GameObject.Find("Minecraft").transform.position.y < -23)
         {
@@ -47,22 +34,10 @@ public class Move : MonoBehaviour {
         }*/
     }
 
-    void CalculateDirection(float h, float v)
-    {
-        angle = Mathf.Atan2(v, h);
-        angle = Mathf.Rad2Deg * angle;
-    }
-
-    void Rotate()
-    {
-        targetrotation = Quaternion.Euler(0, angle, 0);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetrotation, 0.5f);
-    }
-
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Debug.Log("SSS");
-        if(hit.gameObject.tag == "moon")
+        if (hit.gameObject.tag == "moon")
         {
             GameObject.Find("Minecraft").transform.position = new Vector3(18.19f, 2.85f, 4.23f);
             //Invoke("SetPosition",1f);

70f019e7d0b33bd2d84156c465d6dfa49f1f913e
lf29963158 <48079440+lf29963158@users.noreply.github.com>
2019-12-14 15:58:58 +0800

    航空站最新

---
+++ Mars pioneer Hero arise/Assets/SpaceStopFolder/WorldAppear.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldAppear : MonoBehaviour
+{
+    public GameObject canvas;
+    public bool canvasappear = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider NPC)
+    {
+        if (NPC.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                if (!canvasappear)
+                {
+                    canvasappear = true;
+                    canvas.SetActive(true);
+                }
+                else
+                {
+                    canvasappear = false;
+                    canvas.SetActive(false);
+                }
+            }
+        }
+    }
+}

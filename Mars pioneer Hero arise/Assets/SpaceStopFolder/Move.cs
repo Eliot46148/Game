38422505fa0f580930beeb33c59738f4864164d0
joseph871107 <48142918+joseph871107@users.noreply.github.com>
2020-01-02 16:21:24 +0800

    太空人動畫

--- Mars pioneer Hero arise/Assets/SpaceStopFolder/Move.cs
+++ Mars pioneer Hero arise/Assets/SpaceStopFolder/Move.cs
@@ -5,16 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Move : MonoBehaviour
 {
-
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
     Animator anim;
 
+    public bool canMove = true;
+
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GameObject.Find("Minecraft").GetComponent<Animator>();
     }
     void Update()
     {
@@ -25,24 +26,33 @@ public class Move : MonoBehaviour
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(-moveDirection);
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
         }
+
+        if (!canMove)
+            moveDirection = Vector3.zero;
+
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
-        
-        anim.SetInteger("state", 1);
+
+        GameObject.Find("Minecraft").transform.rotation = Quaternion.LookRotation(new Vector3(controller.velocity.x, 0, controller.velocity.z), Vector3.up);
+
+        if (controller.velocity.x + controller.velocity.z != 0)
+            anim.SetInteger("state", 1);
+        else
+            anim.SetInteger("state", 0);
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Debug.Log("SSS");
+        //Debug.Log("SSS");
         if (hit.gameObject.tag == "moon")
         {
-            GameObject.Find("Minecraft").transform.position = new Vector3(18.19f, 2.85f, 4.23f);
+            transform.position = new Vector3(18.19f, 2.85f, 4.23f);
         }
     }
 
